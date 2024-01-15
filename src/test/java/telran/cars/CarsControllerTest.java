b09c98642d8285281c1b20265781883c261c970c
allanteone <allanteone@gmail.com>
2024-01-16 00:34:01 +0200

    hw-53 test

--- src/test/java/telran/cars/CarsControllerTest.java
+++ src/test/java/telran/cars/CarsControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import telran.cars.dto.CarDto;
+import telran.cars.dto.PersonDto;
 import telran.cars.service.CarsService;
 
 @WebMvcTest
@@ -26,23 +27,28 @@ class CarsControllerTest {
 	@Autowired
 	MockMvc mockMvc;
 	CarDto carDto = new CarDto("car", "model");
+	PersonDto personDto = new PersonDto(1, "name", "2000-01-01", "email@email.com");
 	@Autowired
 	ObjectMapper mapper;
 
 	@Test
-	void testAddCar() throws UnsupportedEncodingException, Exception {
+	void testAddCar() throws Exception {
 		when(carsService.addCar(carDto)).thenReturn(carDto);
 		String jsonCarDto = mapper.writeValueAsString(carDto);
-		String actualJSON = mockMvc.perform(post("http://localhost:8080/cars").contentType(MediaType.APPLICATION_JSON)
+		String actualJson = mockMvc.perform(post("http://localhost:8080/cars").contentType(MediaType.APPLICATION_JSON)
 				.content(jsonCarDto)).andExpect(status().isOk()).andReturn().getResponse()
 		.getContentAsString();
-		assertEquals(jsonCarDto, actualJSON);
+		assertEquals(jsonCarDto, actualJson);
 		
 	}
 
 	@Test
-	void testAddPerson() {
-		//TODO
+	void testAddPerson() throws Exception {
+		when(carsService.addPerson(personDto)).thenReturn(personDto);
+		String jsonPersonDto = mapper.writeValueAsString(personDto);
+		String actualJson = mockMvc.perform(post("http://localhost:8080/cars/person").contentType(MediaType.APPLICATION_JSON)
+				.content(jsonPersonDto)).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+		assertEquals(jsonPersonDto, actualJson);
 	}
 
 	@Test

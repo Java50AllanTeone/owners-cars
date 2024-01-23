c06eb83e7bd27fcd3578229e217bee5380f7ac21
allanteone <allanteone@gmail.com>
2024-01-23 22:12:00 +0200

    hw-62

--- src/main/java/telran/cars/dto/ModelDto.java
+++ src/main/java/telran/cars/dto/ModelDto.java
@@ -8,13 +8,10 @@ import static telran.cars.api.ValidationConstants.*;
 @AllArgsConstructor
 public class ModelDto {
 	
-	@NotEmpty(message=MISSING_MODEL_NAME_MESSAGE)
-	String modelName;
+	@NotEmpty(message=MISSING_MODEL_NAME_MESSAGE) String modelName;
 	@NotEmpty(message=MISSING_MODEL_YEAR_MESSAGE)
-	@Min(value=MIN_MODEL_YEAR, message=WRONG_MIN_YEAR)
-	Integer modelYear;
-	@NotEmpty(message=MISSING_COMPANY_MESSAGE)
-	String company;
+	@Min(value=MIN_MODEL_YEAR, message=WRONG_MIN_YEAR) Integer modelYear;
+	@NotEmpty(message=MISSING_COMPANY_MESSAGE) String company;
 	Integer enginePower;
 	Integer engineCapacity;
 }
